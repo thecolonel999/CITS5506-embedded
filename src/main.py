bc02c967b9a5b42548173780cd0010d178bbe18d
David Sanders <thecolonel999@icloud.com>
2020-08-23 17:27:26 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 # Main loop for CITS5506
 # Written by D. Sanders
 
+# IMPORTS
 from functionlib import readBME280, readDS18B20
+from machine import Pin, ADC, RTC, Timer, WDT
+import time
+import ntptime
 
 [temperature, humidity, pressure] = readBME280() # Get the temp, hum and pres values
 
